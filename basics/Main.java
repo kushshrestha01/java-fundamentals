293081f5db64caaf58f9250ff77158efbf36e889
Kush Shrestha <kush_shrestha01@yahoo.com>
2019-05-28 10:28:16 -0700

    Pluralize working

--- basics/Main.java
+++ basics/Main.java
@@ -1,1 +1,21 @@
+public class Main {
+  public static void main(String[] args) {
+    
+    //Pluralize
+    int petcount = 1;
+    String pet = "dog";
+    System.out.println("I own " + petcount + " " + pluralize(pet, petcount) + ".");
 
+    //Flipping Coins
+    
+  }
+
+  public static String pluralize(String word, int a) {
+    if (a == 0 || a > 1) {
+      return (word + "s");
+    } else {
+    return word;
+    }
+  }
+
+}
