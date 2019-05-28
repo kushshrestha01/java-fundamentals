028920fab93686189f982c1f738d73dd08041dc0
Kush Shrestha <kush_shrestha01@yahoo.com>
2019-05-28 11:16:21 -0700

    flipping coins work

--- basics/Main.java
+++ basics/Main.java
@@ -5,9 +5,10 @@ public class Main {
     int petcount = 1;
     String pet = "dog";
     System.out.println("I own " + petcount + " " + pluralize(pet, petcount) + ".");
-
-    //Flipping Coins
     
+    //Flipping Coins
+    flipNHeads(3);
+
   }
 
   public static String pluralize(String word, int a) {
@@ -16,6 +17,27 @@ public class Main {
     } else {
     return word;
     }
+  }
+
+  public static void flipNHeads(int n) { 
+    int numberOfFlips = 0;
+    int numberOfHeads = 0;
+    while(n != numberOfHeads){
+      numberOfFlips++;
+      if(randomNumberGenerator() >= .5){
+        numberOfHeads++;
+        System.out.println("heads");
+      } else {
+        numberOfHeads = 0;
+        System.out.println("tails");
+      }
+    }
+    System.out.println("It took " + numberOfFlips + " flips to flip " + n + " heads in a row.");
+  }
+
+  //Random number
+  public static double randomNumberGenerator(){
+    return Math.random()* 2;
   }
 
 }
