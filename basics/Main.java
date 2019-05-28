4612f7e146ef786ea96702edd368e4423063010b
Kush Shrestha <kush_shrestha01@yahoo.com>
2019-05-28 12:09:11 -0700

    lab complete

--- basics/Main.java
+++ basics/Main.java
@@ -1,3 +1,6 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
   public static void main(String[] args) {
     
@@ -8,6 +11,9 @@ public class Main {
     
     //Flipping Coins
     flipNHeads(3);
+
+    //Command Line Clock
+    clock();
 
   }
 
@@ -35,9 +41,25 @@ public class Main {
     System.out.println("It took " + numberOfFlips + " flips to flip " + n + " heads in a row.");
   }
 
+  public static void clock(){
+    int initialSecond = 0;
+    while(true){
+      LocalDateTime now = LocalDateTime.now();
+      // int hour = now.getHour();
+      // int minute = now.getMinute();
+      int currentSecond = now.getSecond();
+      // or, if you're feeling fancy
+      String time = now.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+      if(initialSecond != currentSecond){
+        System.out.println(time);
+        initialSecond = currentSecond;
+      }
+    }
+  }
+
   //Random number
   public static double randomNumberGenerator(){
     return Math.random()* 2;
   }
-
+  
 }
